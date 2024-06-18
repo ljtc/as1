ccfd8ad6ebe454bdf1b5bc1fc9ef84e653634848
ljtc <ljtc@ciencias.unam.mx>
2024-06-18 11:57:06 -0600

    cuantificadores con conectivos

--- As1/fol.lean
+++ As1/fol.lean
@@ -13,13 +13,12 @@ variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
 --cuantificar x en algo donde no ocurre x lo deja igual
-example : (∃ x : α, r) → r := by
-  intro ⟨_, hr⟩
-  assumption
-
-example (a : α) : r → (∃ x : α, r) := by
-  intro hr
-  exact ⟨a, hr⟩
+example (a : α) : (∃ x : α, r) ↔ r := by
+  constructor
+  . intro ⟨_, hr⟩
+    assumption
+  . intro hr
+    exact ⟨a, hr⟩
 
 example (a : α) : (∀ x : α, r) ↔ r := by
   constructor
@@ -27,9 +26,13 @@ example (a : α) : (∀ x : α, r) ↔ r := by
     exact h a
   . intro hr _
     assumption
---
+---
 
 
+
+/-
+Comportamioento con la conjunción
+-/
 
 -- ∀ con ∧
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
@@ -43,6 +46,14 @@ example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
   . intro ⟨h, h'⟩ x
     exact ⟨h x, h' x⟩
 
+-- ∃ con ∧
+-- sólo se vale una implicación
+example : (∃ x, p x ∧ q x) → (∃ x, p x) ∧ (∃ x, q x) := by
+  intro ⟨a, ⟨pa, qa⟩⟩
+  constructor
+  . exact ⟨a, pa⟩
+  . exact ⟨a, qa⟩
+
 --consecuencia de lo anterior
 example (a : α) : (∀ x, p x ∧ r) ↔ (∀ x, p x) ∧ r := by
   constructor
@@ -54,15 +65,23 @@ example (a : α) : (∀ x, p x ∧ r) ↔ (∀ x, p x) ∧ r := by
   . intro h x
     exact ⟨(h.1) x, h.2⟩
 
+-- caso especial con ∃ y ∧
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
+  constructor
+  . intro ⟨a, h⟩
+    constructor
+    . use a
+      exact h.1
+    . exact h.2
+  . intro ⟨⟨a, hp⟩, hr⟩
+    exact ⟨a, ⟨hp, hr⟩⟩
+---
 
 
--- ∀ con →
--- sólo se vale una implicación
-example : (∀ x, p x → q x) → ((∀ x, p x) → (∀ x, q x)) := by
-  intro h h' a
-  exact (h a) (h' a)
 
-
+/-
+Comportamiento con la disyunción
+-/
 
 -- ∀ con ∨
 -- sólo se vale una implicación
@@ -73,21 +92,6 @@ example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
     exact hp a
   . right
     exact hq a
-
-
-
--- ∃ con ∧ y una fórmula donde x no ocurre
-example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
-  constructor
-  . intro ⟨a, h⟩
-    constructor
-    . use a
-      exact h.1
-    . exact h.2
-  . intro ⟨⟨a, hp⟩, hr⟩
-    exact ⟨a, ⟨hp, hr⟩⟩
-
-
 
 -- ∃ con ∨
 example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
@@ -105,6 +109,99 @@ example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
       right
       assumption
 
+-- consecuencioa de lo anterior
+example (a : α) : (∃ x, p x ∨ r) ↔ (∃ x, p x) ∨ r := by
+  constructor
+  . rintro ⟨b, (pb | hr)⟩
+    . left
+      exact ⟨b, pb⟩
+    . right
+      assumption
+  . rintro (⟨b, pb⟩ | hr)
+    . use b
+      left
+      assumption
+    . use a
+      right
+      assumption
+
+-- caso especial
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  constructor
+  . intro h
+    by_contra h'
+    push_neg at h'
+    rcases h'.1 with ⟨a, hnp⟩
+    rcases (h a) with hp | hr
+    . exact hnp hp
+    . exact h'.2 hr
+  . intro h a
+    rcases h with hp | _
+    . left
+      exact hp a
+    . right
+      assumption
+---
+
+
+
+/-
+Comportamiento con la implicación
+-/
+
+-- ∀ con →
+-- sólo se vale una implicación
+example : (∀ x, p x → q x) → ((∀ x, p x) → (∀ x, q x)) := by
+  intro h h' a
+  exact (h a) (h' a)
+
+
+-- caso especial, consecuente
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := by
+  constructor
+  . intro h ⟨a, hp⟩
+    exact (h a) hp
+  . intro h a hp
+    exact h ⟨a, hp⟩
+
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := by
+  constructor
+  . intro ⟨b, hpr⟩ h
+    exact hpr (h b)
+  . intro h
+    by_contra h'
+    push_neg at h'
+    have : ∀ (x : α), p x := by
+      intro b
+      exact (h' b).1
+    exact (h' a).2 (h this)
+
+-- caso especial, antecedente
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := by
+  constructor
+  . intro ⟨b, hrp⟩ hr
+    use b
+    exact hrp hr
+  . intro h
+    by_contra h'
+    push_neg at h'
+    have : ∃ x, p x := h (h' a).1
+    rcases this with ⟨b, hp⟩
+    exact (h' b).2 hp
+
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+  constructor
+  . intro h hr a
+    exact (h a) hr
+  . intro h a hr
+    exact (h hr) a
+
+
+
+
+/-
+Comportamiento con la negación
+-/
 example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := by
   constructor
   . intro h ⟨a, hnp⟩
@@ -137,59 +234,5 @@ example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := by
   . intro ⟨a, hnp⟩ h
     exact hnp (h a)
 
-example : (∀ x, p x → r) ↔ (∃ x, p x) → r := by
-  constructor
-  . intro h ⟨a, hp⟩
-    exact (h a) hp
-  . intro h a hp
-    exact h ⟨a, hp⟩
-
-example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := by
-  constructor
-  . intro ⟨b, hpr⟩ h
-    exact hpr (h b)
-  . intro h
-    by_contra h'
-    push_neg at h'
-    have : ∀ (x : α), p x := by
-      intro b
-      exact (h' b).1
-    exact (h' a).2 (h this)
-
-example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := by
-  constructor
-  . intro ⟨b, hrp⟩ hr
-    use b
-    exact hrp hr
-  . intro h
-    by_contra h'
-    push_neg at h'
-    have : ∃ x, p x := h (h' a).1
-    rcases this with ⟨b, hp⟩
-    exact (h' b).2 hp
-
-
-example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
-  constructor
-  . intro h
-    by_contra h'
-    push_neg at h'
-    rcases h'.1 with ⟨a, hnp⟩
-    rcases (h a) with hp | hr
-    . exact hnp hp
-    . exact h'.2 hr
-  . intro h a
-    rcases h with hp | _
-    . left
-      exact hp a
-    . right
-      assumption
-
-example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
-  constructor
-  . intro h hr a
-    exact (h a) hr
-  . intro h a hr
-    exact (h hr) a
 
 end fol
