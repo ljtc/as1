cdbbd026ae4ce066a5ba6979bc10247ecae8af50
ljtc <ljtc@ciencias.unam.mx>
2024-06-18 12:23:06 -0600

    contras

--- As1/prop.lean
+++ As1/prop.lean
@@ -1,8 +1,8 @@
 -- Lógica de proposiciones
 
 /-
-El objetivo es formalizar la deducciones lógicas que hemos hecho en el
-curso. Principalmente haremos una demostración de las tautologías.
+Comportamiento estructural de la lógica de proposiciones
+("Álgebra de Boole")
 -/
 
 section LProp
@@ -80,7 +80,7 @@ example : p ∨ (p ∧ q) ↔ p := by
     left
     assumption
 
---distributividad
+--distributividades
 example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
   constructor
   . rintro ⟨hp, (hq | hr)⟩
@@ -95,6 +95,62 @@ example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
     . constructor
       . assumption
       . right; assumption
+
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
+  constructor
+  . rintro (hp | ⟨hq, hr⟩)
+    . constructor
+      repeat' left; assumption
+    . constructor
+      repeat' right; assumption
+  . rintro ⟨(hp | hq), (hp | hr)⟩
+    . left; assumption
+    . left; assumption
+    . left; assumption
+    . right; exact ⟨hq, hr⟩
+
+-- complemento
+example : p ∧ ¬p ↔ False := by
+  constructor
+  . intro ⟨hp, hnp⟩
+    exact hnp hp
+  . intro h
+    exfalso
+    assumption
+
+--esta parte del complemento tiene implicaciones fuertes
+--acerca del tipo de lógica que vamos a trabajar
+example : p ∨ ¬p ↔ True := by
+  constructor
+  . intro _
+    trivial
+  . intro _
+    by_cases h : p
+    . left; assumption
+    . right; assumption
+
+
+
+
+/-
+Métodos de demostración
+-/
+
+--contrapuesta
+example : (p → q) ↔ (¬q → ¬p) := by
+  constructor
+  . intro h hnq hp
+    exact hnq (h hp)
+  . intro h hp
+    by_cases hq : q
+    . assumption
+    . exfalso
+      exact (h hq) hp
+
+--contradicción
+example : ((p → q) ∧ (p → ¬q)) → ¬p := by
+  intro ⟨hpq, hpnq⟩ hp
+  exact (hpnq hp) (hpq hp)
 
 
 end LProp
